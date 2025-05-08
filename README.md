6c41f9be738feef7ba59c6f6f51d36450bb780ec
luhy <luhy@yusur.tech>
2025-05-08 15:36:11 +0800

    Init module.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # edu
 A driver for study linux
+
+make -C /home/luhangyu/code/kernel-devel/kernel-4.18-553/ M=/home/luhangyu/code/edu modules
+
+make -C /home/luhangyu/code/kernel-devel/kernel-4.18-553/ M=/home/luhangyu/code/edu clean
---
+++ edu.c
@@ -0,0 +1,21 @@
+#include<linux/module.h>
+#include<linux/printk.h>
+
+static int __init edu_init(void)
+{
+	printk("A driver for study linux :)\n");
+	return 0;
+}
+
+static void __exit edu_exit(void)
+{
+	printk("Bye bye!\n");
+}
+
+module_init(edu_init);
+module_exit(edu_exit);
+
+MODULE_DESCRIPTION("A driver for study linux");
+MODULE_AUTHOR("luhy");
+MODULE_LICENSE("GPL");
+
